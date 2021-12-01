fd3f9efad08c1905826227bcb1ba78ac0fd960d0
David Tchepak <dave@davesquared.net>
2021-12-02 09:50:29 +1100

    Day01 part 2

--- src/Aoc/Day01.hs
+++ src/Aoc/Day01.hs
@@ -1,10 +1,19 @@
-module Aoc.Day01 (increases, part1) where
+module Aoc.Day01 (increases, window3, part1, part2, input) where
 
 increases :: [Int] -> Int
 increases a = sum (zipWith (\x y -> if y > x then 1 else 0) a (drop 1 a))
 
 part1 :: IO ()
 part1 = input >>= print . increases
+
+window3 :: [Int] -> [(Int, Int, Int)]
+window3 x =
+  zip3 x (drop 1 x) (drop 2 x)
+
+part2 :: [Int] -> Int
+part2 =
+  let sumTriple (x,y,z) = x + y + z
+  in increases . map sumTriple . window3
 
 input :: IO [Int]
 input =
--- test/Aoc/Day01Spec.hs
+++ test/Aoc/Day01Spec.hs
@@ -1,11 +1,24 @@
 module Aoc.Day01Spec (spec) where
 
 import Test.Hspec
-import Aoc.Day01 (increases)
+import Aoc.Day01 (increases, window3, part2)
 
 spec :: Spec
 spec = do
   describe "example data" $ do
-    it "part 1: should find 1721 and 299" $ do
-      let example = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    let example = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    it "part 1: should find 1721 and 299" $
       increases example `shouldBe` 7
+    it "part 2: windows" $
+      window3 example `shouldBe` [
+        (199, 200, 208), -- A
+        (200, 208, 210), -- B
+        (208, 210, 200), -- C
+        (210, 200, 207), -- D
+        (200, 207, 240), -- E
+        (207, 240, 269), -- F
+        (240, 269, 260), -- G
+        (269, 260, 263) -- H
+      ]
+    it "part 2: example" $
+      part2 example `shouldBe` 5
